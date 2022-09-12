d3279812ef0b977e1ccbc1883a988d185392032f
Alexander Belikov <abelikov@gmail.com>
2022-09-12 19:20:15 +0200

    WIP: promoting triple space

--- lm_service/text.py
+++ lm_service/text.py
@@ -3,11 +3,14 @@ from __future__ import annotations
 from itertools import product
 
 from lm_service.graph import transform_advcl
-from lm_service.onto import TripleCandidate
-from lm_service.piles import ExtCandidateList
+from lm_service.onto import TokenIndexT, TripleCandidate
+from lm_service.piles import CandidatePile, ExtCandidateList
 from lm_service.preprocessing import normalize_input_text
 from lm_service.relation import (
-    phrases_to_basis_triples,
+    form_triples,
+    graph_to_candidate_pile,
+    graph_to_maps,
+    text_to_compound_index_graph,
     text_to_coref_sourcetarget,
 )
 
@@ -62,3 +65,55 @@ def text_to_triples(
         return global_triples, phrases_original
     else:
         return global_triples
+
+
+def phrases_to_basis_triples(
+    nlp, rules, phrases
+) -> tuple[
+    list[tuple[TokenIndexT, TokenIndexT, TokenIndexT]],
+    CandidatePile,
+    CandidatePile,
+]:
+    """
+    accumulate prospective triples
+    :param nlp:
+    :param rules:
+    :param phrases:
+    :return:
+    """
+
+    striples = []
+    candidate_depot = CandidatePile()
+    relations = CandidatePile()
+    for k, phrase in enumerate(phrases):
+        (
+            graph_relabeled,
+            rdoc,
+            map_tree_subtree_index,
+        ) = text_to_compound_index_graph(nlp, phrase, initial_phrase_index=k)
+
+        pile, candidate_depot0, mod_graph = graph_to_candidate_pile(
+            graph_relabeled, rules
+        )
+
+        (
+            sources_per_relation,
+            targets_per_relation,
+            rel_sources_per_relation,
+            rel_targets_per_relation,
+            g_undirected,
+        ) = graph_to_maps(mod_graph=mod_graph, pile=pile)
+
+        striples0 = form_triples(
+            pile,
+            sources_per_relation,
+            targets_per_relation,
+            rel_sources_per_relation,
+            rel_targets_per_relation,
+            g_undirected,
+        )
+
+        relations += pile.relations
+        striples += striples0
+        candidate_depot += candidate_depot0
+    return striples, candidate_depot, relations
