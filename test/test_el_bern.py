11abef3310386e57f2a95ffb214139a507182683
Alexander Belikov <abelikov@gmail.com>
2022-10-08 17:37:22 +0200

    upd linking

--- test/test_el_bern.py
+++ test/test_el_bern.py
@@ -1,10 +1,16 @@
 import pkgutil
 import unittest
 
+import coreferee
 import spacy
 import yaml
 
-from lm_service.linking import link_candidate_entity, normalize_bern_entity
+from lm_service.linking import (
+    iterate_linking_over_phrases,
+    link_candidate_entity,
+    normalize_bern_entity,
+)
+from lm_service.onto import MuIndex
 from lm_service.text import normalize_text, phrases_to_basis_triples
 
 
@@ -14,6 +20,29 @@ class MyTestCase(unittest.TestCase):
 
     fp = pkgutil.get_data("lm_service.config", "prune_noun_compound_v2.yaml")
     rules = yaml.load(fp, Loader=yaml.FullLoader)
+
+    response_bern = {
+        "annotations": [
+            {
+                "id": ["mesh:D017719"],
+                "is_neural_normalized": True,
+                "mention": "Diabetic ulcers",
+                "obj": "disease",
+                "prob": 0.9999968409538269,
+                "span": {"begin": 0, "end": 15},
+            },
+            {
+                "id": ["mesh:D002056"],
+                "is_neural_normalized": False,
+                "mention": "burns",
+                "obj": "disease",
+                "prob": 0.9982181191444397,
+                "span": {"begin": 31, "end": 36},
+            },
+        ],
+        "text": "Diabetic ulcers are related to burns.",
+        "timestamp": "Tue Sep 20 16:11:48 +0000 2022",
+    }
 
     def test_bern(self):
         text = "Diabetic ulcers are related to burns."
@@ -28,46 +57,70 @@ class MyTestCase(unittest.TestCase):
 
         ecl = candidate_depot.unfold_conjunction()
 
-        r_bern = {
-            "annotations": [
-                {
-                    "id": ["mesh:D017719"],
-                    "is_neural_normalized": True,
-                    "mention": "Diabetic ulcers",
-                    "obj": "disease",
-                    "prob": 0.9999968409538269,
-                    "span": {"begin": 0, "end": 15},
-                },
-                {
-                    "id": ["mesh:D002056"],
-                    "is_neural_normalized": False,
-                    "mention": "burns",
-                    "obj": "disease",
-                    "prob": 0.9982181191444397,
-                    "span": {"begin": 31, "end": 36},
-                },
-            ],
-            "text": "Diabetic ulcers are related to burns.",
-            "timestamp": "Tue Sep 20 16:11:48 +0000 2022",
-        }
-
-        print(ecl)
-
         # phrase index set
         ix_current_phrase = 0
 
         bern_normalized = [
-            normalize_bern_entity(item) for item in r_bern["annotations"]
+            normalize_bern_entity(item)
+            for item in self.response_bern["annotations"]
         ]
+
+        bern_normalized = [x for x in bern_normalized if x]
+
         map_c2e = link_candidate_entity(
             bern_normalized, ecl, ix_current_phrase
         )
 
         print(map_c2e)
+        self.assertEqual(
+            map_c2e,
+            {
+                MuIndex(False, 0, "001", 0): (0, 0),
+                MuIndex(False, 0, "005", 0): (0, 1),
+            },
+        )
 
-        # r = query_bern(phrase, "v2")
-        # print(r)
-        # print(len(r["entities"]))
+    def test_iterate_linking_over_phrases(self):
+        foo_link = lambda p: self.response_bern["annotations"]
+        text = "Diabetic ulcers are related to burns."
+        phs = normalize_text(text, self.nlp)
+
+        (
+            striples,
+            striples_meta,
+            candidate_depot,
+            relations,
+        ) = phrases_to_basis_triples(self.nlp, self.rules, phs)
+
+        ecl = candidate_depot.unfold_conjunction()
+
+        r = iterate_linking_over_phrases(phrases=phs, ecl=ecl, foo=foo_link)
+        reference = (
+            [
+                {
+                    "linker_type": "bern",
+                    "ent_type": "disease",
+                    "ent_db_type": "mesh",
+                    "id": "D017719",
+                    "confidence": 0.9999968409538269,
+                    "span": (0, 15),
+                },
+                {
+                    "linker_type": "bern",
+                    "ent_type": "disease",
+                    "ent_db_type": "mesh",
+                    "id": "D002056",
+                    "confidence": 0.9982181191444397,
+                    "span": (31, 36),
+                },
+            ],
+            {
+                MuIndex(meta=False, phrase=0, token="001", running=0): (0, 0),
+                MuIndex(meta=False, phrase=0, token="005", running=0): (0, 1),
+            },
+        )
+
+        self.assertEqual(r, reference)
 
 
 if __name__ == "__main__":
