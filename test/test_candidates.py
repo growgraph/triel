450f25f8797ca9ff4a4c4268ac82cd8024343f10
Alexander Belikov <abelikov@gmail.com>
2026-03-09 16:31:51 +0100

    WIP: tests updated

--- test/test_candidates.py
+++ test/test_candidates.py
@@ -5,6 +5,7 @@ test candidate extraction
 import logging
 from collections import deque
 
+from triel.coref import text_to_compound_index_graph
 from triel.graph import phrase_to_deptree
 from triel.onto import (
     ACandidateKind,
@@ -118,10 +119,12 @@ def test_st_candidates(documents, nlp_fixture, rules_v3):
 
 def test_relation_subtree_dfs(documents, nlp_fixture, rules_v3):
     piles = []
-    vertices_of_interest = [3, 22, 1]
+    vertices_of_interest = [(0, "003"), (0, "022"), (0, "001")]
     vertices_of_interest = [deque([(x, 0)]) for x in vertices_of_interest]
     for deq, document in zip(vertices_of_interest, documents[1:]):
-        rdoc, graph = phrase_to_deptree(nlp_fixture, document)
+        graph, _, _ = text_to_compound_index_graph(
+            nlp_fixture, document, initial_phrase_index=0, single_phrase_mode=True
+        )
         ograph = graph.copy()
         cr = Relation()
         find_subtree_dfs(graph, ograph, deq, cr, rules=rules_v3["relation"])
@@ -131,18 +134,20 @@ def test_relation_subtree_dfs(documents, nlp_fixture, rules_v3):
     assert [len(rp) for rp in piles] == [3, 2, 1]
 
     assert {k: p.stokens for k, p in enumerate(piles)} == {
-        0: ["002", "003", "004"],
-        1: ["021", "022"],
-        2: ["001"],
+        0: [(0, "002"), (0, "003"), (0, "004")],
+        1: [(0, "021"), (0, "022")],
+        2: [(0, "001")],
     }
 
 
 def test_st_subtree_dfs(documents, nlp_fixture, rules_v3):
     piles = []
-    vertices_of_interest = [9, 24, 2]
+    vertices_of_interest = [(0, "009"), (0, "024"), (0, "002")]
     vertices_of_interest = [deque([(x, 0)]) for x in vertices_of_interest]
     for deq, document in zip(vertices_of_interest, documents[1:]):
-        rdoc, graph = phrase_to_deptree(nlp_fixture, document)
+        graph, _, _ = text_to_compound_index_graph(
+            nlp_fixture, document, initial_phrase_index=0, single_phrase_mode=True
+        )
         original_graph = graph.copy()
         st = SourceOrTarget()
         find_subtree_dfs(
@@ -158,22 +163,22 @@ def test_st_subtree_dfs(documents, nlp_fixture, rules_v3):
     assert [len(rp) for rp in piles] == [5, 12, 1]
 
     assert {k: p.stokens for k, p in enumerate(piles)} == {
-        0: ["005", "006", "007", "008", "009"],
+        0: [(0, "005"), (0, "006"), (0, "007"), (0, "008"), (0, "009")],
         1: [
-            "023",
-            "024",
-            "025",
-            "026",
-            "027",
-            "028",
-            "029",
-            "030",
-            "031",
-            "032",
-            "033",
-            "034",
+            (0, "023"),
+            (0, "024"),
+            (0, "025"),
+            (0, "026"),
+            (0, "027"),
+            (0, "028"),
+            (0, "029"),
+            (0, "030"),
+            (0, "031"),
+            (0, "032"),
+            (0, "033"),
+            (0, "034"),
         ],
-        2: ["002"],
+        2: [(0, "002")],
     }
 
 
@@ -216,29 +221,32 @@ def test_consecutive_candidates(documents, nlp_fixture, rules_v3):
 
 def test_sort_index_tree(nlp_fixture, rules_v3, documents):
     tokens = [
-        Token(**{"s": 2, "text": "a0", "successors": {1, 0}}),
-        Token(**{"s": 1, "text": "a1", "predecessors": {2}}),
+        Token(s=(0, "002"), text="a0", successors={(0, "001"), (0, "000")}),
+        Token(s=(0, "001"), text="a1", predecessors={(0, "002")}),
         Token(
-            **{
-                "s": 0,
-                "text": "a2",
-                "predecessors": {2},
-                "successors": {15},
-            }
+            s=(0, "000"),
+            text="a2",
+            predecessors={(0, "002")},
+            successors={(0, "015")},
         ),
         Token(
-            **{
-                "s": 15,
-                "text": "b0",
-                "predecessors": {2},
-                "successors": {16, 17},
-            }
+            s=(0, "015"),
+            text="b0",
+            predecessors={(0, "002")},
+            successors={(0, "016"), (0, "017")},
         ),
-        Token(**{"s": 16, "text": "b1", "predecessors": {15}}),
-        Token(**{"s": 17, "text": "b2", "predecessors": {15}}),
+        Token(s=(0, "016"), text="b1", predecessors={(0, "015")}),
+        Token(s=(0, "017"), text="b2", predecessors={(0, "015")}),
     ]
     ac = Candidate().from_tokens(tokens)
 
     ac.sort_index()
 
-    assert ac.stokens == ["000", "015", "016", "017", "001", "002"]
+    assert ac.stokens == [
+        (0, "000"),
+        (0, "015"),
+        (0, "016"),
+        (0, "017"),
+        (0, "001"),
+        (0, "002"),
+    ]
--- test/test_flag.py
+++ test/test_flag.py
@@ -7,12 +7,12 @@ logger = logging.getLogger(__name__)
 
 
 def test_flag(rules_v2):
-    t = Token(**{"s": 7, "text": "his", "dep_": "conj", "tag_": "VBG"})
+    t = Token(s=7, text="his", dep_="conj", tag_="VBG")
     flag = get_flag(t.__dict__, rules_v2["source_target"]["secondary"])
     assert flag
 
 
 def test_flag_secondary(rules_v3):
-    t = Token(**{"s": 7, "text": "supress", "dep_": "xcomp", "tag_": "VB"})
+    t = Token(s=7, text="supress", dep_="xcomp", tag_="VB")
     flag = get_flag(t.__dict__, rules_v3["relation"]["secondary"])
     assert flag
